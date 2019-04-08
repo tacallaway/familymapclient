4a264d604f2079e36f2d68d2f573ab5c0a3ccc91
tacallaway <46659840+tacallaway@users.noreply.github.com>
2019-04-07 18:37:40 -0600

    Add files via upload

--- app/src/main/java/com/example/familymapclient/MainActivity.java
+++ app/src/main/java/com/example/familymapclient/MainActivity.java
@@ -1,20 +1,13 @@
 package com.example.familymapclient;
 
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements LoginFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
     }
 }
